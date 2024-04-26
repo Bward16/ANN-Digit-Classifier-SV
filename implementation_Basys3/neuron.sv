575f20a8c6c414ddd9a516c96c057dabaa834eda
Blake Ward <blake.ward42@gmail.com>
2024-04-25 19:13:55 -0700

    parameterized inputs and added bias

--- implementation_Basys3/neuron.sv
+++ implementation_Basys3/neuron.sv
@@ -1,25 +1,21 @@
 
-module (
-    input logic [31:0] a,
-    input logic [31:0] b,
-    input logic [31:0] c,
-    input logic [31:0] d,
-
-    input logic [31:0] w0,
-    input logic [31:0] w1,
-    input logic [31:0] w2,
-    input logic [31:0] w3,
-
+module #( parameter N = 4) (
+    input logic [31:0] x[N],
+    input logic [31:0] w[N],
+    input logic [31:0] bias,
     input logic [31:0] sigmoid,
-
     output logic [31:0] y
 );
 
     logic [31:0] sum;
 
-    always_comb begin 
+    always_comb begin
 
-        sum = a * w0 + b * w1 + c * w2 + d * w3;
+        sum = bias;
+        
+        for (int i = 0; i < N; i++) begin
+            sum += x[i] * w[i];
+        end
 
         y = (sum > sigmoid) ? 1 : 0;
 
