6165fe4b2b53ab7103427f7932bcaa9628ad5c3d
Bward16 <115198013+Bward16@users.noreply.github.com>
2024-05-01 17:08:58 -0700

    removed ugly vivado comment block

--- implementation_Basys3/neuron.sv
+++ implementation_Basys3/neuron.sv
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/29/2024 02:39:41 PM
-// Design Name: 
-// Module Name: neuron
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module Simple_Neuron#( 
     parameter INPUT_COUNT = 4, 
     parameter[31:0] weights [INPUT_COUNT] = {0, 0, 0, 0},
--- implementation_Basys3/simple_tests.sv
+++ implementation_Basys3/simple_tests.sv
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/30/2024 04:16:21 PM
-// Design Name: 
-// Module Name: simple_tests
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module simple_tests(
 
     );
