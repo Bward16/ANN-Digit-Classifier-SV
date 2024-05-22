9bdd39674fc1d107bac609ef5884611981e9fcfe
jnardone <jnardone@calpoly.edu>
2024-05-21 22:07:47 -0700

    Added integer inputs for MNIST dataset

--- MNIST/genMNISTinput.py
+++ MNIST/genMNISTinput.py
@@ -1,24 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from tensorflow.keras.datasets import mnist
 
-# Load MNIST dataset
-mnist = tf.keras.datasets.mnist
+
 (train_images, train_labels), (_, _) = mnist.load_data()
 
 # Select a random image from the dataset
 random_index = np.random.randint(0, train_images.shape[0])
-random_image = train_images[random_index]
-random_image_label = train_labels[random_index]
+
+static_index = 1234
+
+random_image = train_images[static_index]
+random_image_label = train_labels[static_index]
 
 # Display the image
 plt.imshow(random_image)
 
 # Save the image to a file
 plt.savefig('mnist_input_image.png')
-
-# Rescale pixel values to the range [0, 1]
-random_image = (random_image / 255.0)
 
 # Save the pixel values to a text file
 with open("mnist.txt", "w") as file:
